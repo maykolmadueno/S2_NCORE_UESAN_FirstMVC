15a073a17be44aa606390b8cd0930a44d8229b2b
MRGREEN <20100843@ue.edu.pe>
2023-02-17 00:31:41 -0500

    Agregar archivos de proyecto.

---
+++ S2_NCORE_UESAN_FirstMVC.Web/Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S2_NCORE_UESAN_FirstMVC.Web.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
