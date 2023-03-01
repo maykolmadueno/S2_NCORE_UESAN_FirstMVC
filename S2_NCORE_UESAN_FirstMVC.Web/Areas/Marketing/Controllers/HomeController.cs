21b77c6d342d235ea1555093fee9e5264bc81ba8
20100843@ue.edu.pe <20100843@ue.edu.pe>
2023-02-28 19:34:00 -0500

    ViewModel in Marketing

---
+++ S2_NCORE_UESAN_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using S2_NCORE_UESAN_FirstMVC.Web.Areas.Marketing.Models;
+
+namespace S2_NCORE_UESAN_FirstMVC.Web.Areas.Marketing.Controllers
+{
+    [Area("Marketing")]
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductsViewModel()
+        {
+            var products= GetProductsJsonLocal();
+            return View(products);
+        }
+
+        public IEnumerable<Product> GetProductsJsonLocal()
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\\products.json");
+            var json = System.IO.File.ReadAllText(folder);
+
+            var products= JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+            return products;
+        }
+    }
+}
---
+++ S2_NCORE_UESAN_FirstMVC.Web/Areas/Marketing/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace S2_NCORE_UESAN_FirstMVC.Web.Areas.Marketing.Models
+{
+    public class Product
+    {
+       public string Id { get; set; }
+
+       public string Maker { get; set; }
+       public string Img { get; set; }
+       public string Url { get; set; }
+       public string Title { get; set; }
+
+       public string Description { get; set; }
+
+
+
+
+    }
+}
