fea923792c88a2f786e9a873fe8e78ce3e869a55
20100843@ue.edu.pe <20100843@ue.edu.pe>
2023-02-28 20:20:28 -0500

    ültima actualización

--- S2_NCORE_UESAN_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ S2_NCORE_UESAN_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace S2_NCORE_UESAN_FirstMVC.Web.Areas.Marketing.Controllers
             var products= GetProductsJsonLocal();
             return View(products);
         }
-<<<<<<< HEAD
+
         public IActionResult ProductsViewData()
         {
             var products = GetProductsJsonLocal();
@@ -31,8 +31,7 @@ namespace S2_NCORE_UESAN_FirstMVC.Web.Areas.Marketing.Controllers
             ViewBag.ProductList = products;
             return View();
         }
-=======
->>>>>>> 21b77c6d342d235ea1555093fee9e5264bc81ba8
+
 
         public IEnumerable<Product> GetProductsJsonLocal()
         {
